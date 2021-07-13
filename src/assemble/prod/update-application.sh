caece798456c4bea948f0e8ba41ecdea9c551929
Nuno Freire <nfreire@gmailcom>
2021-07-13 11:58:57 +0100

    fix scrip actualizaçao

--- src/assemble/prod/update-application.sh
+++ src/assemble/prod/update-application.sh
@@ -2,13 +2,17 @@
 rm -rf lib
 rm -rf classes/rossio
 rm -rf classes/org/oclc
+rm -rf git
 
 mkdir backup
 cp -r data backup
 cp *.sh backup
 
-unzip -o ingestao-metadados-LinhaDeComandos.zip
+git clone https://github.com/fcshrossio/ingestao-metadados.git git
+cd git
+mvn -P prod assembly:assembly
+cd ..
+
+unzip -o git/target/ingestao-metadados-LinhaDeComandos.zip
 
 chmod u+x *.sh
-
-
