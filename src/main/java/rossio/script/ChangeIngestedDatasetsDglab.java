8f9f8e89b177378f65a47157be71adb073e9cfdb
nfreire <nfreire@gmail.com>
2022-05-20 11:49:10 +0100

    correcao subject - description dglab

--- src/main/java/rossio/script/ChangeIngestedDatasetsDglab.java
+++ src/main/java/rossio/script/ChangeIngestedDatasetsDglab.java
@@ -3,11 +3,13 @@ package rossio.script;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.HashSet;
 import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFParser;
 import org.oclc.oai.harvester2.verb.ListSets;
@@ -15,6 +17,8 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import rossio.data.models.Dc;
+import rossio.data.models.DcTerms;
 import rossio.data.models.Dcat;
 import rossio.data.models.Rdf;
 import rossio.data.models.Rossio;
@@ -31,9 +35,21 @@ import rossio.util.XmlUtil;
 public class ChangeIngestedDatasetsDglab {
 
 	public static void main(String[] args) throws Exception {
+		HashSet<String> dataproviderUris=new HashSet<String>();
+//		dataproviderUris.add("http://vocabs.rossio.fcsh.unl.pt/agentes/c_5672cf6f");
+		dataproviderUris.add("http://vocabs.rossio.fcsh.unl.pt/agentes/c_4229e047");
+		dataproviderUris.add("http://vocabs.rossio.fcsh.unl.pt/agentes/c_f9e05c96");
+		dataproviderUris.add("http://vocabs.rossio.fcsh.unl.pt/agentes/c_5062b87f");
+		dataproviderUris.add("http://vocabs.rossio.fcsh.unl.pt/agentes/c_50a5323c");
+		dataproviderUris.add("http://vocabs.rossio.fcsh.unl.pt/agentes/c_6da5082f");
+		dataproviderUris.add("http://vocabs.rossio.fcsh.unl.pt/agentes/c_819ef5cf");
+		dataproviderUris.add("http://vocabs.rossio.fcsh.unl.pt/agentes/c_53e2aa91");
+		
 		String filename="src/data/oai_sources_debug.ttl";
 		String solrRepoUrl="http://192.168.111.170:8983/solr/repositorio";
-		String solrRepoUrlOut="http://datarossio.dglab.gov.pt:8983/solr/testes-repositorio";
+//		String solrRepoUrlOut="http://datarossio.dglab.gov.pt:8983/solr/repositorio";
+		String solrRepoUrlOut=null;
+		
 		final boolean testing;
 		if (args!=null && args.length>=2) {
 			filename=args[0];
@@ -42,36 +58,58 @@ public class ChangeIngestedDatasetsDglab {
 			testing=false;
 		} else
 			testing = true;
+//			testing=false;
 
-    	RepositoryWithSolr repository=new RepositoryWithSolr(solrRepoUrl);
+		RepositoryWithSolr repository=new RepositoryWithSolr(solrRepoUrl);
     	RepositoryWithSolr repositoryOut;
-    	if(solrRepoUrlOut==null)
+    	if(solrRepoUrlOut==null || solrRepoUrl.equals(solrRepoUrlOut))
     		repositoryOut=repository;
     	else
     		repositoryOut=new RepositoryWithSolr(solrRepoUrl);
     	
 		OaiSources oaiSources=new OaiSources(new File(filename));
 		for(OaiSource s: oaiSources.getAllSources()) {
-			if (s.preprocessor!=null && s.preprocessor instanceof DglabPreprocessor) {
+			if (dataproviderUris.contains(s.dataProvider)) {
+				System.out.println("Processing "+s.getSourceId());
 				repository.getItemsInSource(s.getSourceId(), FetchOption.BOTH_VERSIONS, new ItemHandler() {
 					int recCnt=0;
+					int recSkip=0;
 					@Override
 					public boolean handle(String uuid, String identifierAtSource, String lastHarvestTimestamp, byte[] contentAtSource, byte[] contentRossio)
 							throws Exception {
-						RDFParser reader = RDFParser.create().lang(Lang.RDFTHRIFT).source(new ByteArrayInputStream(contentRossio)).build();
+						RDFParser reader = RDFParser.create().lang(Lang.RDFTHRIFT).source(new ByteArrayInputStream(contentAtSource)).build();
 						Model model = Jena.createModel();
 						reader.parse(model);
+						Resource cho = model.createResource(Rossio.NS_ITEM+uuid);
+						
+//						RdfUtil.printOutRdf(model);
+						
+						Statement subject = cho.getProperty(DcTerms.subject);
+						if(subject!=null) {
 
-						DglabPreprocessor.moveSubjectToDescription(model.createResource(Rossio.NS_ITEM+uuid));
-						
-						repositoryOut.updateItem(uuid, s.getSourceId(), identifierAtSource, contentAtSource, 
-								RdfUtil.serializeToRdfRift(model));
-						
-						recCnt++;
+//							RdfUtil.printOutRdf(model);
+							DglabPreprocessor.moveSubjectToDescription(cho);
+//System.out.println("--->");
+//							RdfUtil.printOutRdf(model);
+//							System.out.println("EEEEEEEEEEEEEEEEEEEEENNNNNNNNNDDDDDDDDDDD");
+							
+							
+							repositoryOut.updateItem(uuid, s.getSourceId(), identifierAtSource, RdfUtil.serializeToRdfRift(model), contentRossio);
+							recCnt++;
 //						if(recCnt % 20000==0)
-						if(recCnt % 20==0)
-							repositoryOut.commit();
-						return testing ? recCnt<1000 : true;
+							if(recCnt % 20==0) 
+								System.out.println("updated rec "+uuid +" ("+recCnt+")");
+							if(recCnt % 100==0) {
+								repositoryOut.commit();
+								System.out.println("commit"); 
+							}
+							return testing ? recCnt<20 : true;
+						} else {
+							recSkip++;
+							if(recSkip % 1000==0)
+								System.out.println("Skipped "+recSkip);
+						}
+						return true;
 					}
 				});
 				repositoryOut.commit();
